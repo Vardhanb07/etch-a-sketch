11c365d432c10f838d77249609ec88a618cdc54c
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-01-22 19:42:17 +0530

    Add code to produce 32 divs and contain them in container class div

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-const firstContainer = document.querySelector('.firstContainer');
-const scondContainer = document.querySelector('.secondContainer');
-const thirdContainer = document.querySelector('.thirdContainer');
-const fourthContainer = document.querySelector('.fourthContainer');
+const container = document.querySelector('.container');
 
-function boxCreate(container) {
-    for(let i = 0; i < 4; i++){
-        container.appendChild((document.createElement('div')));
-        
-    }
+for(let i = 0; i < 32; i++){
+    let div = document.createElement('div');
+    div.classList.add(`${i + 1}`);
+    container.appendChild(div);
 }
-
-boxCreate(firstContainer);
-boxCreate(scondContainer);
-boxCreate(thirdContainer);
-boxCreate(fourthContainer);
